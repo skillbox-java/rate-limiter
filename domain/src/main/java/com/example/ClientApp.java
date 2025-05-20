c71694cd33f10ea1d8533a5570d35b2b25dcf50b
Konstantin Shibkov <sendel@sendel.ru>
2025-05-21 00:00:31 +0500

    add exceptions

--- domain/src/main/java/com/example/ClientApp.java
+++ domain/src/main/java/com/example/ClientApp.java
@@ -1,6 +1,25 @@
 package com.example;
 
 
+import com.example.exception.ValidationException;
+
+import java.util.Objects;
 import java.util.UUID;
 
-public record ClientApp(UUID id, String name, String apiKey) {}
+public record ClientApp(UUID id, String name, String apiKey) {
+
+    public ClientApp {
+        if(Objects.isNull(id)){
+            throw new ValidationException("id не может быть null");
+        }
+
+        if(Objects.isNull(name)){
+            throw new ValidationException("name не может быть null");
+        }
+
+        if(name.isBlank()){
+            throw new ValidationException("name не может быть пустым");
+        }
+
+    }
+}
